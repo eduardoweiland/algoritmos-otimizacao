93762545e4f600e60597ab68a00199304cce7b5a
Eduardo Weiland <eduardo@eduardoweiland.info>
2016-06-11 00:00:21 -0300

    Caixeiro viajante finalizado

--- README.md
+++ README.md
@@ -16,3 +16,25 @@ O programa espera ler a configuração do grafo (nodos, arestas e valores) da en
 
 A saída do programa é direcionada à saída padrão, e contém um log descrevendo todos os passos executados pelo algoritmo.
 
+
+## Caixeiro Viajante (tsp)
+
+Para compilar o código do problema do caixeiro viajante não é necessária nenhuma biblioteca adicional. A maneira mais simples de compilar o projeto todo é com o comando
+
+    $ gcc *.c -o tsp
+
+O programa espera ler os nodos do problema da entrada padrão. Para executá-lo utilizando um arquivo como fonte de entrada dos dados, pode-se utilizar o comando
+
+    $ ./tsp < /caminho/do/arquivo.txt
+
+A saída do programa é direcionada à saída padrão. Por padrão, apenas o resultado final é exibido. Para visualizar todos os passos executados pelo algoritmo, pode ser informada a flag verbose **-v** na execução do programa:
+
+    $ ./tsp -v < /caminho/do/arquivo.txt
+
+
+## TODO
+
+[ ] Makefiles para compilação dos programas
+[ ] Organizar saída do programa max\_flow (adicionar opção verbose)
+[ ] Descrever formatos dos arquivos de entrada utilizados
+
--- tsp/main.c
+++ tsp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 #include "graph.h"
@@ -8,40 +9,78 @@
 void process_input(graph_t *graph);
 void print_path(const path_t *path);
 
-int main(void)
+int main(int argc, char **argv)
 {
     graph_t graph;
+    path_t best, candidate, best_candidate;
+    int rot_size, rot_start;
+    int verbose = 0;
+
+    if ((argc == 2) && (strncmp(argv[1], "-v", sizeof("-v")) == 0)) {
+        verbose = 1;
+    }
+    else if (argc > 1) {
+        fprintf(stderr, "\n");
+        fprintf(stderr, "  Parâmetros inválidos!\n");
+        fprintf(stderr, "\n");
+        fprintf(stderr, "Uso: %s [-v]\n", argv[0]);
+        fprintf(stderr, " -v     Ativa modo verbose (exibe detalhes da "
+                        "execução do algoritmo)\n");
+        return EXIT_FAILURE;
+    }
 
     process_input(&graph);
     graph_calculate_distances(&graph);
 
-    path_t path, rotate1;
-    path_init(&path, &graph);
-    path_find_greedy(&path, 1);
+    path_init(&best, &graph);
+    path_find_greedy(&best, 1);
 
-    printf("Caminho inicial: ");
-    print_path(&path);
-
-    // TODO: gerar todas possibilidades e manter sempre a melhor
-    printf("\n");
-    for (int i = 1; i < 37; ++i) {
-        path_copy(&rotate1, &path);
-        path_rotate(&rotate1, 2, i);
-        printf("Rotação #%02d    : ", i);
-        print_path(&rotate1);
-        path_destroy(&rotate1);
+    if (verbose) {
+        printf("1o. caminho: ");
+        print_path(&best);
     }
 
-    printf("\n");
-    for (int i = 1; i < 36; ++i) {
-        path_copy(&rotate1, &path);
-        path_rotate(&rotate1, 3, i);
-        printf("Rotação #%02d    : ", i);
-        print_path(&rotate1);
-        path_destroy(&rotate1);
+    for (rot_size = 2; rot_size < graph.node_count; ++rot_size) {
+        if (verbose) {
+            printf("\n\n===== Trocando caminhos com tamanho %2d =====\n", rot_size);
+        }
+
+        path_copy(&best_candidate, &best);
+
+        for (rot_start = 1; rot_start <= graph.node_count - rot_size; ++rot_start) {
+            path_copy(&candidate, &best);
+            path_rotate(&candidate, rot_size, rot_start);
+            if (verbose) {
+                printf("Troca # %3d: ", rot_start);
+                print_path(&candidate);
+            }
+
+            if (candidate.distance < best_candidate.distance) {
+                path_copy(&best_candidate, &candidate);
+            }
+
+            path_destroy(&candidate);
+        }
+
+        if (best_candidate.distance < best.distance) {
+            path_copy(&best, &best_candidate);
+        }
+
+        if (verbose) {
+            printf("\n   MELHOR  : ");
+            print_path(&best);
+        }
     }
 
-    path_destroy(&path);
+    if (verbose) {
+        printf("\n============================================\n");
+    }
+
+    printf("Melhor caminho encontrado:\n");
+    print_path(&best);
+
+    path_destroy(&best);
+    path_destroy(&best_candidate);
     graph_destroy(&graph);
 
     return EXIT_SUCCESS;
@@ -76,5 +115,5 @@ void print_path(const path_t *path)
         printf("%3d ", path->nodes[i]->number);
     }
 
-    printf("-> Distância: %.2lf\n", path->distance);
+    printf(" ->  Distância: %lf\n", path->distance);
 }
